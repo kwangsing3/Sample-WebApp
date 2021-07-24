e591207bb1a6d2953cba789795f8e12e0a754b1e
kwangsing3 <kwangsing3@gmail.com>
2021-07-24 17:48:44 +0800

    Add: adding serve HTML file sample.

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	router "WebApp/src"
 	"fmt"
 	"net/http"
-
-	router "WebApp/src"
+	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 var port = ":1337"
+var Flag = int8(0)
+var AcceptFlags = []string{`-dev`, `-pro`} // 0:Debug Mode、1:Production mode
 
 func main() {
+
+	// Configure launch mode
+	argnames := os.Args
+	msg := ``
+	if len(argnames) == 2 {
+		for i := range AcceptFlags {
+			Flag = map[bool]int8{true: int8(i), false: 0}[strings.EqualFold(argnames[1], AcceptFlags[i])]
+		}
+	} else {
+		for i := range AcceptFlags {
+			msg += `[` + AcceptFlags[i] + `] `
+		}
+		fmt.Println(`This application can run with one args tag. Option: ` + msg)
+	}
+
+	msg = `Start to run with flag:` + AcceptFlags[Flag]
+	fmt.Println(msg)
+
+	// Setting Router
 	r := mux.NewRouter()
-	router.SetRouter(r) //Setting website Path
+	router.SetRouter(r, Flag) //Setting website Path
 
 	fmt.Println("Server Listen at: " + port)
 	http.ListenAndServe(port, r)
--- src/router.go
+++ src/router.go
@@ -2,17 +2,25 @@ package router
 
 import (
 	"net/http"
+	"path"
 
 	"github.com/gorilla/mux"
 )
 
+var flagMode = int8(0)
+
 //SetRouter 設定路由
-func SetRouter(r *mux.Router) error {
-	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte(`<font color="red">Hello red world!</font>`))
-	})
+func SetRouter(r *mux.Router, flag int8) error {
+	flagMode = flag
+	r.HandleFunc("/", homeHandler)
 	/*
-		r.HandleFunc("/products", ProductsHandler)
+		r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+			w.Write([]byte(`<font color="red">Hello red world!</font>`))
+		})
 	*/
 	return nil
+}
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	//w.Write([]byte(`<font color="red">Hello red world!</font>`))
+	http.ServeFile(w, req, path.Join(`res/index.html`))
 }
